deec00cec72d8aba3ae118c75cfe783da896e975
hercilioln <nuneshercilio@gmail.com>
2020-04-19 16:07:03 -0300

    Subindo Arquivos

---
+++ python_aula/new2.py
@@ -0,0 +1,53 @@
+'''
+soma de dois numeros
+num1 = int(input())
+num2 = int(input())
+print (num1 + num2)
+
+'''
+'''
+#converter Farenhait em Celcius
+F = int(input())
+C = (5*(F-32)/9)
+print(C)'''
+
+'''num1 = int(input())
+num2 = int(input()) #numero inteiro
+numR = float(input()) #numero real usar float
+prod = num1 * 2 * (num2/2)
+print('o produto do dobro com a metade do segundo é', prod) #sempre colocar virgula depois das aspas
+tripsome = num1 * 3 + numR
+print("a soma do triplo:", tripsome)
+cube = numR**3
+print('o terceiro elevado ao cubo:', cube)'''
+
+'''
+largura = int(input("Largura do quadrado: "))
+altura = int(input("Altura do quadrado: "))
+
+area = largura * altura
+print(area)
+print ((area * 2))
+'''
+''''
+file = int(input("Qual o tamanho do arquivo em MB? "))
+link = int(input("Qual a velocidade do link em Mbps?"))
+time = ((file * 8)/ link) / 60
+
+print("The time of download is:", time, "minutos")
+''''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
