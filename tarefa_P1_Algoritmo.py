94adb9e33f15256b7cb992b300eb0a05ffa7ca5c
hercilio <nuneshercilio@gmail.com>
2020-05-05 00:47:46 -0300

    Novas tarefas

---
+++ tarefa_P1_Algoritmo.py
@@ -0,0 +1,53 @@
+'''Elabore um algoritmo para testar se uma senha digita é igual a “batatafrita”. Se a senha estiver correta
+escreva “Acesso permitido”, do contrario emita a mensagem “Você não tem acesso ao sistema”.'''
+
+'''senha = input("Digite a senha:")
+
+if(senha == "batatafrita"):
+    print("Acesso Permitido")
+else:
+    print("Você não tem acesso ao sistema") '''
+
+'''Um comerciante comprou um produto e quer vendê-lo com 
+um lucro de 45% se o valor da compra
+for menor que R$ 20,00; caso contrário, o lucro será de 
+30%. Elabore um algoritmo que leia o valor
+do produto e imprima o valor de venda para o produto.'''
+
+'''produto = int(input("Valor do produto: "))
+if produto <= 20:
+  valor1 = (45/100+1) * produto
+  print("Valor de venda do produto: {}".format(valor1))
+elif produto  > 20:
+  valor2 = (30/100+1) * produto
+  print("Valor de venda do produto: {}".format(valor2))'''
+
+#Criar um algoritmo que receba o valor de x, e calcule e imprima o valor de f(x).
+
+'''x = (int(input("Digite um valor para x: ")))
+y = (5*x+3)/(x**2-16)**0.5
+print('O valor de f(x) =', y) '''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
